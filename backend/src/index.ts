6cf95ea884a89d697556df7de92d3c594cae68b0
Ajeeth Shinde <89684651+Ajeeth-07@users.noreply.github.com>
2025-03-05 19:41:06 +0530

    cors updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,10 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://tms-rfllm1zbz-ajeeths-projects-b190ef2b.vercel.app", 'http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
