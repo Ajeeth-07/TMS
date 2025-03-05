52434dae4ec4a549625329ac60ab1e84df8ce701
Ajeeth Shinde <89684651+Ajeeth-07@users.noreply.github.com>
2025-03-05 12:40:24 +0530

    auth pages done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 
 function App() {
 
   return (
-    <div>
-      hi there
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        {/* TODO:Other routes will be added later */}
+      </Routes>
+    </Router>
   )
 }
 
